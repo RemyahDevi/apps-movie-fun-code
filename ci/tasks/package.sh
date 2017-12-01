f31891f473a1659932f0296c15b317917c90ffc0
Remyah <remyah@ctsusbrwboulder.cts.com>
2017-12-01 16:36:13 +0530

    war change

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -7,14 +7,14 @@ pushd movie-fun-source
   ./mvn clean package -DskipTests
 popd
 
-jar_count=`find movie-fun-source/target -type f -name *.jar | wc -l`
+jar_count=`find movie-fun-source/target -type f -name *.war | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find movie-fun-source/target -type f -name *.jar -exec cp "{}" package-output/movie-fun.jar \;
+find movie-fun-source/target -type f -name *.war -exec cp "{}" package-output/movie-fun.war \;
 
 echo "Done packaging"
 exit 0
